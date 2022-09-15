6562d228d16e1b76b3f1f5045470af62047f59a2
Leks2000 <107043945+Leks2000@users.noreply.github.com>
2022-09-16 00:58:36 +0300

    Add files via upload

---
+++ SetCursor/snak.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Prom
+{
+    internal class Snak
+    {
+        public int snak;
+        public string str;
+        int simb = 0;
+
+        int x = (Console.WindowWidth / 2);
+        int y = (Console.WindowHeight / 3);
+
+
+        public string BOut(int length, char simbol)
+        {
+            simb = length;
+            str = new string(simbol, simb);
+            return str;
+        }
+
+        public void bocm(ref int x,ref int y)
+        {   
+            for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, y++))
+            {
+                Console.WriteLine(BOut(7, '█'));
+            }
+
+            Console.SetCursorPosition(x+=2, y-=5);
+            Console.Write(BOut(3, ' '));
+            Console.SetCursorPosition(x, y+=2);
+            Console.WriteLine(BOut(3, ' '));
+
+        }
+        public void tri(ref int x, ref int y)
+        {
+            for (int j = 0; j < 6; j++, Console.SetCursorPosition(x, y++))
+            {
+                Console.WriteLine(BOut(7, '█'));
+            }
+        }
+    }
+}
